c2f0a85849023e0e7fca8a669ba879af8d2c956e
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-05-08 20:40:05 +0530

    update

---
+++ Frontend/src/Components/index.js
@@ -0,0 +1,20 @@
+import BookingCard from "./BookingCard";
+import CommentSection from "./CommentSection";
+import Company_card from "./Company_card"
+import CustomCalendar from "./CustomCalendar";
+import Event_card from "./Event_card";
+import Footer from "./Footer";
+import FreqtQuestion from "./FreqtQuestion"
+import Gallery_Card from "./Gallery_card";
+import Header from "./Header";
+import ImageLoader1 from "./ImageLoader1";
+import ImageLoader2 from "./ImageLoader2";
+import Loader from "./loader";
+import Navbar from "./Navbar";
+import Slider from "./Slider";
+import UpcomingEvents from "./UpcomingEvents";
+import Venue_card from "./Venue_card";
+import VenueProfile from "./VenueProfile";
+import VirtualCard from "./VirtualCard";
+
+export { BookingCard, CommentSection, Company_card, CustomCalendar, Event_card, Footer, FreqtQuestion, Gallery_Card, Header, ImageLoader1, ImageLoader2, Loader, Navbar, Slider, UpcomingEvents, Venue_card, VenueProfile, VirtualCard }
