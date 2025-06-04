b50cb41a9b633d233c5f3f443a613a330ce0f29e
Keya Tarafdar <keya.tarafdar2003@gmail.com>
2025-06-04 21:49:50 +0530

    update

--- Backend/controller/venueController.js
+++ Backend/controller/venueController.js
@@ -12,6 +12,7 @@ const {
   errorResponse_badRequest,
   errorResponse_catchError,
   successResponse_ok,
+  errorResponse_alreadyExists,
 } = require("../responseObject");
 
 // Register Venue
@@ -39,7 +40,7 @@ module.exports.signUp = async (req, res) => {
     ) {
       const existingVenue = await venueModel.findOne({ email });
       if (existingVenue) {
-        return res.send("Venue already exists. Please Login.");
+        return errorResponse_alreadyExists(res,"Venue already exists!")
       }
 
       // const apiUrl = `https://api.zerobounce.net/v2/validate?api_key=${
