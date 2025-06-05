63ca0f25ee6df45baae128fd1e13ac6a52bd4da0
Keya Tarafdar <keya.tarafdar2003@gmail.com>
2025-06-06 01:27:48 +0530

    accept reject venue fixed

--- Backend/controller/adminController.js
+++ Backend/controller/adminController.js
@@ -270,52 +270,52 @@ module.exports.acceptVenue = async (req, res) => {
 
     await userModel.findOneAndDelete({ email: venue.email });
 
-    //const testAccount = await nodemailer.createTestAccount();
+    const testAccount = await nodemailer.createTestAccount();
 
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.ethereal.email",
-    //   port: 587,
-    //   auth: {
-    //     user: process.env.user,
-    //     pass: process.env.pass,
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: process.env.user,
+        pass: process.env.pass,
+      },
+    });
 
-    //     await transporter.sendMail({
-    //       from: '"Eventek" <eventek@gmail.com>',
-    //       to: venue.email,
-    //       subject: "Application accepted",
-    //       html: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f9f9f9; color: #333;">
-    //     <div style=" margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #ddd;">
-    //         <div style="background-color: #4CAF50; color: #ffffff; padding: 15px; border-radius: 8px 8px 0 0; text-align: center;">
-    //             <h2 style="margin: 0;">Venue Application Approved</h2>
-    //         </div>
-    //         <div style="padding: 20px;">
-    //             <p>Dear ${venue.name},</p>
-    //             <p>We are pleased to inform you that your venue application has been <strong>approved</strong>! Congratulations on joining our platform. Your venue will soon be available for event creators to explore and book.</p>
-    //             <p>To ensure smooth operation and maximize your visibility, we kindly request you to complete your profile to 100%. A fully completed profile helps event creators make informed decisions and enhances your chances of securing bookings.</p>
-    //             <p><strong>Your login credentials are as follows:</strong></p>
-    //             <ul style="padding-left: 20px;">
-    //                 <li><b>Email:</b> ${venue.email}</li>
-    //                 <li><b>Password:</b> ${venue.temporaryPassword}</li>
-    //             </ul>
-    //             <p><strong>Note:</strong> For your security, please change your password immediately after your first login.</p>
-    //             <div style="text-align: center; margin-top: 20px;">
-    //                 <a href="http://localhost:5173/resetpassword/${venueId}" target="_blank"
-    //                    style="background-color: #007BFF; color: #ffffff; text-decoration: none; padding: 10px 20px; font-size: 20px; border-radius: 8px; display: inline-block; cursor: pointer;">Change Password</a>
-    //             </div>
-    //             <p style="margin-top: 20px;">If you have any questions or need assistance, feel free to contact our support team. We are here to help you make the most of our platform.</p>
-    //             <p>Once again, welcome aboard! We look forward to a successful partnership.</p>
-    //         </div>
-    //         <div style="margin-top: 20px; font-size: 14px; color: #777; text-align: center; padding: 10px 0; border-top: 1px solid #ddd;">
-    //             <p>Warm regards,<br>The Eventek Team</p>
-    //             <p><i>Your success is our priority.</i></p>
-    //         </div>
-    //     </div>
-    // </body>`,
-    //     });
+    await transporter.sendMail({
+      from: '"Eventek" <eventek@gmail.com>',
+      to: venue.email,
+      subject: "Application accepted",
+      html: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f9f9f9; color: #333;">
+        <div style=" margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #ddd;">
+            <div style="background-color: #4CAF50; color: #ffffff; padding: 15px; border-radius: 8px 8px 0 0; text-align: center;">
+                <h2 style="margin: 0;">Venue Application Approved</h2>
+            </div>
+            <div style="padding: 20px;">
+                <p>Dear ${venue.name},</p>
+                <p>We are pleased to inform you that your venue application has been <strong>approved</strong>! Congratulations on joining our platform. Your venue will soon be available for event creators to explore and book.</p>
+                <p>To ensure smooth operation and maximize your visibility, we kindly request you to complete your profile to 100%. A fully completed profile helps event creators make informed decisions and enhances your chances of securing bookings.</p>
+                <p><strong>Your login credentials are as follows:</strong></p>
+                <ul style="padding-left: 20px;">
+                    <li><b>Email:</b> ${venue.email}</li>
+                    <li><b>Password:</b> ${venue.temporaryPassword}</li>
+                </ul>
+                <p><strong>Note:</strong> For your security, please change your password immediately after your first login.</p>
+                <div style="text-align: center; margin-top: 20px;">
+                    <a href="http://localhost:5173/resetpassword/${venueId}" target="_blank"
+                       style="background-color: #007BFF; color: #ffffff; text-decoration: none; padding: 10px 20px; font-size: 20px; border-radius: 8px; display: inline-block; cursor: pointer;">Change Password</a>
+                </div>
+                <p style="margin-top: 20px;">If you have any questions or need assistance, feel free to contact our support team. We are here to help you make the most of our platform.</p>
+                <p>Once again, welcome aboard! We look forward to a successful partnership.</p>
+            </div>
+            <div style="margin-top: 20px; font-size: 14px; color: #777; text-align: center; padding: 10px 0; border-top: 1px solid #ddd;">
+                <p>Warm regards,<br>The Eventek Team</p>
+                <p><i>Your success is our priority.</i></p>
+            </div>
+        </div>
+    </body>`,
+    });
 
-    if (!process.env.SENDGRID_API_KEY) {
+    /*if (!process.env.SENDGRID_API_KEY) {
       return res.status(500).send("SendGrid API key not configured.");
     }
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -352,10 +352,10 @@ module.exports.acceptVenue = async (req, res) => {
             </div>
         </div>
     </body>`,
-    });
-    res.send("Venue is added and email sent.");
+    });*/
+    return successResponse_ok(res, "Venue is added and email sent", null);
   } catch (err) {
-    res.status(500).send(err.message);
+    return errorResponse_catchError(res, err.message);
   }
 };
 
@@ -366,49 +366,78 @@ module.exports.rejectVenue = async (req, res) => {
     let venue = await venueModel.findOneAndDelete({ _id: venueId });
     await adminModel.updateMany({}, { $pull: { appliedVenues: venueId } });
 
-    //const testAccount = await nodemailer.createTestAccount();
+    const testAccount = await nodemailer.createTestAccount();
 
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.ethereal.email",
-    //   port: 587,
-    //   auth: {
-    //     user: process.env.user,
-    //     pass: process.env.pass,
-    //   },
-    // });
-
-    await sgMail.send({
-      to: venue.email,
-      from: '"Eventek" <eventek@gmail.com>',
-      subject: "Venue Application rejected",
-      html: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f9f9f9; color: #333;">
-    <div style="margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #ddd;">
-        <div style="background-color: #f6440e; color: #ffffff; padding: 15px; border-radius: 8px 8px 0 0; text-align: center;">
-            <h2 style="margin: 0;">Venue Application Rejected</h2>
-        </div>
-        <div style="padding: 20px;">
-            <p>Dear ${venue.name},</p>
-            <p>We regret to inform you that your venue application has been <strong>rejected</strong> after careful review. Unfortunately, it did not meet all the necessary criteria for approval.</p>
-            
-            <p style="background-color: #FFF4E5; color: #D84315; padding: 10px; border-left: 4px solid #FF5722; margin: 20px 0; border-radius: 4px;">
-                <strong>Reason for Rejection:</strong> ${reason}
-            </p>
-
-            <p>We encourage you to review your application and make the necessary improvements or provide additional documentation if applicable. You are welcome to reapply once the required changes have been made.</p>
-            <p>If you would like more details about the reasons for rejection or need assistance, feel free to contact our support team. We are here to help you through the process and provide any guidance you may need.</p>
-            <p>Thank you for your interest in our platform. We appreciate the effort you put into your application and hope to see you again in the future.</p>
-        </div>
-        <div style="margin-top: 20px; font-size: 14px; color: #777; text-align: center; padding: 10px 0; border-top: 1px solid #ddd;">
-            <p>Warm regards,<br>The Eventek Team</p>
-            <p><i>Your success is our priority.</i></p>
-        </div>
-    </div>
-</body>`,
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: process.env.user,
+        pass: process.env.pass,
+      },
     });
 
-    res.send("Venue is rejected");
+    await transporter.sendMail({
+      from: '"Eventek" <eventek@gmail.com>',
+      to: venue.email,
+      subject: "Application accepted",
+      html: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f9f9f9; color: #333;">
+         <div style="margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #ddd;">
+             <div style="background-color: #f6440e; color: #ffffff; padding: 15px; border-radius: 8px 8px 0 0; text-align: center;">
+                 <h2 style="margin: 0;">Venue Application Rejected</h2>
+             </div>
+             <div style="padding: 20px;">
+                 <p>Dear ${venue.name},</p>
+                 <p>We regret to inform you that your venue application has been <strong>rejected</strong> after careful review. Unfortunately, it did not meet all the necessary criteria for approval.</p>
+
+                 <p style="background-color: #FFF4E5; color: #D84315; padding: 10px; border-left: 4px solid #FF5722; margin: 20px 0; border-radius: 4px;">
+                     <strong>Reason for Rejection:</strong> ${reason}
+                 </p>
+
+                 <p>We encourage you to review your application and make the necessary improvements or provide additional documentation if applicable. You are welcome to reapply once the required changes have been made.</p>
+                 <p>If you would like more details about the reasons for rejection or need assistance, feel free to contact our support team. We are here to help you through the process and provide any guidance you may need.</p>
+                 <p>Thank you for your interest in our platform. We appreciate the effort you put into your application and hope to see you again in the future.</p>
+             </div>
+             <div style="margin-top: 20px; font-size: 14px; color: #777; text-align: center; padding: 10px 0; border-top: 1px solid #ddd;">
+                 <p>Warm regards,<br>The Eventek Team</p>
+                 <p><i>Your success is our priority.</i></p>
+             </div>
+         </div>
+     </body>`,
+    });
+
+    //     await sgMail.send({
+    //       to: venue.email,
+    //       from: '"Eventek" <eventek@gmail.com>',
+    //       subject: "Venue Application rejected",
+    //       html: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f9f9f9; color: #333;">
+    //     <div style="margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); border: 1px solid #ddd;">
+    //         <div style="background-color: #f6440e; color: #ffffff; padding: 15px; border-radius: 8px 8px 0 0; text-align: center;">
+    //             <h2 style="margin: 0;">Venue Application Rejected</h2>
+    //         </div>
+    //         <div style="padding: 20px;">
+    //             <p>Dear ${venue.name},</p>
+    //             <p>We regret to inform you that your venue application has been <strong>rejected</strong> after careful review. Unfortunately, it did not meet all the necessary criteria for approval.</p>
+
+    //             <p style="background-color: #FFF4E5; color: #D84315; padding: 10px; border-left: 4px solid #FF5722; margin: 20px 0; border-radius: 4px;">
+    //                 <strong>Reason for Rejection:</strong> ${reason}
+    //             </p>
+
+    //             <p>We encourage you to review your application and make the necessary improvements or provide additional documentation if applicable. You are welcome to reapply once the required changes have been made.</p>
+    //             <p>If you would like more details about the reasons for rejection or need assistance, feel free to contact our support team. We are here to help you through the process and provide any guidance you may need.</p>
+    //             <p>Thank you for your interest in our platform. We appreciate the effort you put into your application and hope to see you again in the future.</p>
+    //         </div>
+    //         <div style="margin-top: 20px; font-size: 14px; color: #777; text-align: center; padding: 10px 0; border-top: 1px solid #ddd;">
+    //             <p>Warm regards,<br>The Eventek Team</p>
+    //             <p><i>Your success is our priority.</i></p>
+    //         </div>
+    //     </div>
+    // </body>`,
+    //     });
+
+    return successResponse_ok(res, "Venue is rejected", null);
   } catch (err) {
-    res.status(500).send(err.message);
+    return errorResponse_catchError(res, err.message);
   }
 };
 
